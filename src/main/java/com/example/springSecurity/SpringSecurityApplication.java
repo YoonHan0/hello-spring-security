37129ef10a5837410ec61f69f5b905bb5e9c245a
YoonHan0 <dbsgksdud12@naver.com>
2025-10-28 17:51:16 +0900

    project init

---
+++ src/main/java/com/example/springSecurity/SpringSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.example.springSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+
+		System.out.println("=== Spring Security 프로젝트 실행 ===");
+	}
+
+}
