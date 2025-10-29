7c9fed6bf39a12effa9321adabe5d850937f29b5
YoonHan0 <dbsgksdud12@naver.com>
2025-10-29 18:01:38 +0900

    멀티 SecurityFilterChain 등록

--- src/main/java/com/example/springSecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springSecurity/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.springSecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,14 +13,38 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    @Order(1)
     public SecurityFilterChain filterChain1(HttpSecurity http) throws Exception {
+
+        // "/user" 경로에 대해서만 맵핑이 됨 (필수)
+        http
+                .securityMatchers((auth) -> auth.requestMatchers("/user"));
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/user").permitAll());
 
         return http.build();
     }
 
     @Bean
+    @Order(2)
     public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
 
+        // "/admin" 경로에 대해서만 맵핑이 됨 (필수)
+        http
+                .securityMatchers((auth) -> auth.requestMatchers("/admin"));
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/admin").permitAll());
+
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+
+        return web -> web.ignoring().requestMatchers("/img/**");
     }
 }
