c714e8995c198f55aa529b3b002ef2b6658c9327
YoonHan0 <dbsgksdud12@naver.com>
2025-10-30 00:38:38 +0900

    filter 호출 테스트를 위한 controller 예시 작성

--- src/main/java/com/example/springSecurity/controller/MainController.java
+++ src/main/java/com/example/springSecurity/controller/MainController.java
@@ -1,4 +1,22 @@
 package com.example.springSecurity.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class MainController {
+
+    @GetMapping("/user")
+    public String userPage() {
+
+        return "USER PAGE";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+
+        return "ADMIN PAGE";
+    }
 }
